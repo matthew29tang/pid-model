40d1404b719ddcd9d1687af43f11eda86a289534
matthew29tang <matthew29tang@gmail.com>
2020-05-03 00:53:51 -0700

    Updates

---
+++ precache-manifest.9c9a0867ef5560f52560f5f363dd969e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b8c46f31cf8d5dc18334b11d4dc6b6",
+    "url": "/pid-model/index.html"
+  },
+  {
+    "revision": "e3a6ba9aa7f649ea3f59",
+    "url": "/pid-model/static/css/main.2c1ae835.chunk.css"
+  },
+  {
+    "revision": "8fc1843c133c15cfc6e3",
+    "url": "/pid-model/static/js/2.b445c1af.chunk.js"
+  },
+  {
+    "revision": "e3a6ba9aa7f649ea3f59",
+    "url": "/pid-model/static/js/main.20ab9101.chunk.js"
+  },
+  {
+    "revision": "b5b5b1ac35b4381f8095",
+    "url": "/pid-model/static/js/runtime~main.acca72a3.js"
+  },
+  {
+    "revision": "e8946904f01674b12e8f1ad05bba14dc",
+    "url": "/pid-model/static/media/colab_rounded.e8946904.png"
+  },
+  {
+    "revision": "78543f49057dddae8e016688d51a9cd0",
+    "url": "/pid-model/static/media/densenet.78543f49.png"
+  },
+  {
+    "revision": "a2bd1c9ebdb37566e09fc3e6a278646b",
+    "url": "/pid-model/static/media/modelA.a2bd1c9e.png"
+  },
+  {
+    "revision": "08138b99d284f985a01d92bde4e035c3",
+    "url": "/pid-model/static/media/modelB.08138b99.png"
+  },
+  {
+    "revision": "c4a8b1ee62fcd3746dd4645bc0fc90c2",
+    "url": "/pid-model/static/media/modelC.c4a8b1ee.png"
+  }
+]);
