f768c5df4a863422c432eb361c3f1f0ccfd85070
matthew29tang <matthew29tang@gmail.com>
2020-05-05 14:04:26 -0700

    Add skeleton gifs

--- src/pages/Pose.js
+++ src/pages/Pose.js
@@ -10,12 +10,12 @@ import styles from './commonStyles.js';
 class Pose extends React.Component {
   constructor(props) {
     super(props);
-    this.images = [
-      require('./images/modelC.png'),
-      require('./images/yolo.png'),
-      require('./images/sort.png'),
-      require('./images/pose.png'),
-      require('./images/densenet.png'),
+    this.gifs = [
+      require('./gifs/skeleton1.gif'),
+      require('./gifs/skeleton2.gif'),
+      require('./gifs/skeleton3.gif'),
+      require('./gifs/skeleton4.gif'),
+
     ];
   }
 
@@ -28,10 +28,32 @@ class Pose extends React.Component {
           <Divider />
           <br />
           <div className={classes.body}>
-          The skeleton fitting algorithm takes an image as input and returns up to 17 keypoints for the pedestrian. Out of these 17 keypoints, 9 were chosen to be most important for the classification. 396 features were then calculated, consisting of angles and distances between these 9 keypoints and the horizontal plane. For this implementation, the BB’s from the object detector was used to crop each image prior to the skeleton fitting algorithm so that each image only portrayed one pedestrian.
+            This work was primarly done by the team at Chalmers University and was integrated into the Berkeley system at the end.The skeleton fitting algorithm takes an image as input and returns up to 17 keypoints for the pedestrian. Out of these 17 keypoints, 9 were chosen to be most important for the classification. 396 features were then calculated, consisting of angles and distances between these 9 keypoints and the horizontal plane. For this implementation, the BB’s from the object detector was used to crop each image prior to the skeleton fitting algorithm so that each image only portrayed one pedestrian.
           </div>
           <br /><br />
-          
+          <Grid container spacing={3}>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>
+                <img src={this.gifs[0]} alt="1" width="100%" />
+              </Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>
+                <img src={this.gifs[1]} alt="2" width="100%" />
+              </Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>
+                <img src={this.gifs[2]} alt="3" width="100%" />
+              </Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>
+                <img src={this.gifs[3]} alt="4" width="100%" />
+              </Paper>
+            </Grid>
+          </Grid>
+
         </Paper>
       </div>
     );
