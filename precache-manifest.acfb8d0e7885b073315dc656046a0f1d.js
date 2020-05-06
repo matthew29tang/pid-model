c86da5c34ad54485998a8f6097ec5ab5a85e6a7f
matthew29tang <matthew29tang@gmail.com>
2020-05-05 17:05:33 -0700

    Updates

---
+++ precache-manifest.acfb8d0e7885b073315dc656046a0f1d.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5bee2c33edacd61c0c34c2d75571d1",
+    "url": "/pid-model/index.html"
+  },
+  {
+    "revision": "82d9a410464063f9827e",
+    "url": "/pid-model/static/css/main.2c1ae835.chunk.css"
+  },
+  {
+    "revision": "1733ff11c7dd55d96297",
+    "url": "/pid-model/static/js/2.9822611c.chunk.js"
+  },
+  {
+    "revision": "82d9a410464063f9827e",
+    "url": "/pid-model/static/js/main.60c9836f.chunk.js"
+  },
+  {
+    "revision": "b5b5b1ac35b4381f8095",
+    "url": "/pid-model/static/js/runtime~main.acca72a3.js"
+  },
+  {
+    "revision": "e8946904f01674b12e8f1ad05bba14dc",
+    "url": "/pid-model/static/media/colab_rounded.e8946904.png"
+  },
+  {
+    "revision": "5b2b9c39e953a98a2b2a5e73cef5f091",
+    "url": "/pid-model/static/media/deepsort1.5b2b9c39.gif"
+  },
+  {
+    "revision": "437fb576a66730d2407b007a62e1a88b",
+    "url": "/pid-model/static/media/deepsort2.437fb576.gif"
+  },
+  {
+    "revision": "78543f49057dddae8e016688d51a9cd0",
+    "url": "/pid-model/static/media/densenet.78543f49.png"
+  },
+  {
+    "revision": "112f77857d43450ea67c67c46d23e92d",
+    "url": "/pid-model/static/media/densenetTable1.112f7785.png"
+  },
+  {
+    "revision": "390508a4316ba7d423740b0efcbb979a",
+    "url": "/pid-model/static/media/densenetTable2.390508a4.png"
+  },
+  {
+    "revision": "a9f78759d3acb54902a920bf68623c7c",
+    "url": "/pid-model/static/media/dnetFig1.a9f78759.jpg"
+  },
+  {
+    "revision": "60dd2d7d28119ab43a52dfab33d4dc23",
+    "url": "/pid-model/static/media/dnetFig2.60dd2d7d.png"
+  },
+  {
+    "revision": "a2bd1c9ebdb37566e09fc3e6a278646b",
+    "url": "/pid-model/static/media/modelA.a2bd1c9e.png"
+  },
+  {
+    "revision": "cb03bfe4e67a4c6feae1c30cda6e7698",
+    "url": "/pid-model/static/media/modelA1.cb03bfe4.gif"
+  },
+  {
+    "revision": "41a1b6cc5dc891b907b182614538c0d9",
+    "url": "/pid-model/static/media/modelA2.41a1b6cc.gif"
+  },
+  {
+    "revision": "19e14cc49d9b56bbcf233857462068b9",
+    "url": "/pid-model/static/media/modelA3.19e14cc4.gif"
+  },
+  {
+    "revision": "9d2c69ef4346793dadee2ac9bbd7bf3d",
+    "url": "/pid-model/static/media/modelA4.9d2c69ef.gif"
+  },
+  {
+    "revision": "58a7bcf1c440b895fb2686804405e3b5",
+    "url": "/pid-model/static/media/modelADetect.58a7bcf1.gif"
+  },
+  {
+    "revision": "08138b99d284f985a01d92bde4e035c3",
+    "url": "/pid-model/static/media/modelB.08138b99.png"
+  },
+  {
+    "revision": "6c1ae136ac9d59c915f54cf501511034",
+    "url": "/pid-model/static/media/modelB1.6c1ae136.gif"
+  },
+  {
+    "revision": "521c60d8f3913cdeafd6d6e847bd7187",
+    "url": "/pid-model/static/media/modelB2.521c60d8.gif"
+  },
+  {
+    "revision": "69e88c9dfc60b08418ef7ace87899eeb",
+    "url": "/pid-model/static/media/modelB3.69e88c9d.gif"
+  },
+  {
+    "revision": "6483a0d91dc9950cd3acf64be4621388",
+    "url": "/pid-model/static/media/modelB4.6483a0d9.gif"
+  },
+  {
+    "revision": "9ec1043f45f8c8507e9a9c0e72724d7d",
+    "url": "/pid-model/static/media/modelBDetect.9ec1043f.gif"
+  },
+  {
+    "revision": "c4a8b1ee62fcd3746dd4645bc0fc90c2",
+    "url": "/pid-model/static/media/modelC.c4a8b1ee.png"
+  },
+  {
+    "revision": "a3a58daf97488aa3965ce023c56f3ab5",
+    "url": "/pid-model/static/media/skeleton1.a3a58daf.gif"
+  },
+  {
+    "revision": "967c268501d6f9b693a7354f45063a64",
+    "url": "/pid-model/static/media/skeleton2.967c2685.gif"
+  },
+  {
+    "revision": "254f532758b6eafcc2ee7872f80a67bd",
+    "url": "/pid-model/static/media/skeleton3.254f5327.gif"
+  },
+  {
+    "revision": "8a41b3a1d2e4897dbbbef1f0a180cdc2",
+    "url": "/pid-model/static/media/skeleton4.8a41b3a1.gif"
+  },
+  {
+    "revision": "3f44c2123a51e5577daa5726d55673b7",
+    "url": "/pid-model/static/media/sort1.3f44c212.gif"
+  },
+  {
+    "revision": "a9eee8e9ca85a1ed3f951f771c29f0da",
+    "url": "/pid-model/static/media/sort2.a9eee8e9.gif"
+  },
+  {
+    "revision": "56f8924267d0ffb17b9ad9cd656b6889",
+    "url": "/pid-model/static/media/yolo1.56f89242.gif"
+  },
+  {
+    "revision": "a6cfaa39b09054087889778d11ba34eb",
+    "url": "/pid-model/static/media/yolo2.a6cfaa39.gif"
+  }
+]);
