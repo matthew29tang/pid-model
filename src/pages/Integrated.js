c0b3bb2e8cbaaf89ac82286e0288d192ed918119
matthew29tang <matthew29tang@gmail.com>
2020-05-12 16:59:28 -0700

    Add model C and D results

--- src/pages/Integrated.js
+++ src/pages/Integrated.js
@@ -26,6 +26,7 @@ class Integrated extends React.Component {
       require('./images/modelA.png'),
       require('./images/modelB.png'),
       require('./images/modelC.png'),
+      require('./images/modelD.png'),
     ];
     this.gifs = [[
       require('./gifs/modelA1.gif'),
@@ -37,11 +38,28 @@ class Integrated extends React.Component {
       require('./gifs/modelB2.gif'),
       require('./gifs/modelB3.gif'),
       require('./gifs/modelB4.gif'),
-    ]
-    ];
-    this.resultGifs = [
-      require('./gifs/modelADetect.gif'),
-      require('./gifs/modelBDetect.gif')
+    ], [
+      require('./gifs/modelC1.gif'),
+      require('./gifs/modelC2.gif'),
+      require('./gifs/modelC3.gif'),
+      require('./gifs/modelC4.gif'),
+    ], [
+      require('./gifs/modelD1.gif'),
+      require('./gifs/modelD2.gif'),
+      require('./gifs/modelD3.gif'),
+      require('./gifs/modelD4.gif'),
+    ]];
+    this.acComparison = [[
+      require('./gifs/modelA_cmp1.gif'),
+      require('./gifs/modelA_cmp2.gif'),
+      require('./gifs/modelA_cmp3.gif')
+    ], [
+      require('./gifs/modelC_cmp1.gif'),
+      require('./gifs/modelC_cmp2.gif'),
+      require('./gifs/modelC_cmp3.gif')
+    ]];
+    this.resultImages = [
+      require('./images/metrics.png')
     ]
     this.state = {
       tab: 0,
@@ -85,6 +103,7 @@ class Integrated extends React.Component {
               <Tab label="Model A" />
               <Tab label="Model B" />
               <Tab label="Model C" />
+              <Tab label="Model D" />
               <Tab label="Results" />
 
             </Tabs>
@@ -138,7 +157,7 @@ class Integrated extends React.Component {
                   </Grid>
 
                   <br />
-                  <Colab desc="Model A" link="https://colab.research.google.com/drive/1qztt4iC0kNcJKVF5o1pbefdEWnP9VwcL?usp=sharing" />
+                  <Colab desc="Model A" link="https://colab.research.google.com/drive/1InGYCg_SEosqumijaGyWzC9VoLPm5ZEx#scrollTo=DGgcEDqHNyUl&forceEdit=true&sandboxMode=true" />
                 </div> : ''}
             </TabContainer>
 
@@ -178,7 +197,7 @@ class Integrated extends React.Component {
                     </Grid>
                   </Grid>
                   <br />
-                  <Colab desc="Model B" link="https://colab.research.google.com/drive/1AOx2hzWeQceorhu75Zvc0esZLw0SzlRH?usp=sharing" />
+                  <Colab desc="Model B" link="https://colab.research.google.com/drive/19YmKS3KZvl86tDN4CWDEZ7lPWjFWWrQQ#scrollTo=SU6qWoWr1QpP&forceEdit=true&sandboxMode=true" />
                   <br /><br />
                 </div> : ''}
             </TabContainer>
@@ -191,8 +210,33 @@ class Integrated extends React.Component {
                   </Paper>
                   <br />
                   <div className={classes.body}>
-                    Model C consists of 4 components: YOLO, DeepSORT, Skeleton Tracking, and Densenet. This integrated framework combined the work from students at Chalmers University on using pose estimation for pedestrian intent detection with our (UC Berkeley) framework to achieve higher accuracies.
+                    Model C consists of 4 components: YOLO, SORT, Skeleton Tracking, and Densenet. This integrated framework combined the work from students at Chalmers University on using pose estimation for pedestrian intent detection with our (UC Berkeley) framework to achieve higher accuracies.
                   </div>
+                  <br />
+                  <Grid container spacing={3}>
+                    <Grid item xs={6}>
+                      <Paper className={classes.paper}>
+                        <img src={this.gifs[2][0]} alt="modelC1" width="90%" />
+                      </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Paper className={classes.paper}>
+                        <img src={this.gifs[2][1]} alt="modelC2" width="90%" />
+                      </Paper>
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={3}>
+                    <Grid item xs={6}>
+                      <Paper className={classes.paper}>
+                        <img src={this.gifs[2][2]} alt="modelC3" width="90%" />
+                      </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Paper className={classes.paper}>
+                        <img src={this.gifs[2][3]} alt="modelC4" width="90%" />
+                      </Paper>
+                    </Grid>
+                  </Grid>
                   <br />
                   <Colab desc="Model C" link="https://colab.research.google.com/drive/1qztt4iC0kNcJKVF5o1pbefdEWnP9VwcL?usp=sharing" />
                   <br /><br />
@@ -201,6 +245,47 @@ class Integrated extends React.Component {
 
             <TabContainer >
               {this.state.tab === 3 ?
+                <div>
+                  <Paper className={classes.paper}>
+                    <img src={this.images[3]} alt="modelD" width="100%" />
+                  </Paper>
+                  <br />
+                  <div className={classes.body}>
+                    Model D consists of 4 components: YOLO, DeepSORT, Skeleton Tracking, and Densenet. This integrated framework combined the work from students at Chalmers University on using pose estimation for pedestrian intent detection with our (UC Berkeley) framework to achieve higher accuracies.
+                  </div>
+                  <br />
+                  <Grid container spacing={3}>
+                    <Grid item xs={6}>
+                      <Paper className={classes.paper}>
+                        <img src={this.gifs[3][0]} alt="modelD1" width="90%" />
+                      </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Paper className={classes.paper}>
+                        <img src={this.gifs[3][1]} alt="modelD2" width="90%" />
+                      </Paper>
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={3}>
+                    <Grid item xs={6}>
+                      <Paper className={classes.paper}>
+                        <img src={this.gifs[3][2]} alt="modelD3" width="90%" />
+                      </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Paper className={classes.paper}>
+                        <img src={this.gifs[3][3]} alt="modelD4" width="90%" />
+                      </Paper>
+                    </Grid>
+                  </Grid>
+                  <br />
+                  <Colab desc="Model D" link="https://colab.research.google.com/drive/1qztt4iC0kNcJKVF5o1pbefdEWnP9VwcL?usp=sharing" />
+                  <br /><br />
+                </div> : ''}
+            </TabContainer>
+
+            <TabContainer >
+              {this.state.tab === 4 ?
                 <div>
                   <h2 style={{ marginBottom: "0px", marginTop: "0px" }}>Side by Side Comparison</h2>
                   <h5 style={{ marginTop: "5px", marginBottom: "10px" }}>Note: Click the Video tabs to resync the GIFs if they fall out of sync.</h5>
@@ -215,54 +300,40 @@ class Integrated extends React.Component {
                       <Tab label="Video 1" />
                       <Tab label="Video 2" />
                       <Tab label="Video 3" />
-                      <Tab label="Video 4" />
 
                     </Tabs>
                   </AppBar>
                   <br />
                   <Grid container spacing={3}>
-                    <Grid item xs={4}>
+                    <Grid item xs={6}>
                       <Paper className={classes.paper}>
-                        <img src={this.gifs[0][this.state.innerTab]} alt="modelA2" width="100%" />
+                        <img src={this.acComparison[0][this.state.innerTab]} alt="modelA" width="90%" /> <br />
+                        Model A
                       </Paper>
                     </Grid>
-                    <Grid item xs={4}>
+                    <Grid item xs={6}>
                       <Paper className={classes.paper}>
-                        <img src={this.gifs[1][this.state.innerTab]} alt="modelB2" width="100%" />
-                      </Paper>
-                    </Grid>
-                    <Grid item xs={4}>
-                      <Paper className={classes.paper}>
-                        <img src={this.gifs[1][this.state.innerTab]} alt="modelB2" width="100%" />
+                        <img src={this.acComparison[1][this.state.innerTab]} alt="modelC" width="90%" /> <br />
+                        Model C
                       </Paper>
                     </Grid>
                   </Grid>
                   <br />
                   <h2>Results</h2>
                   <div className={classes.body}>
-                    We used three different metrics to anaylze Model A's performance. The first involved using the accuracy of the model's prediction of a pedestrian's intent 16 frames (with videos running at 30fps, around half a second) before they cross which was 0.345. The second involved using the accuracy of the model's prediction of a pedestrian's intent at the frame at which they actually begin crossing, which was 0.479. The third involved using the percentage accuracy of the model's prediction of a pedestrian's intent 16 frames before, which was 0.468.
+                    <ol>
+                      <li>M1: The accuracy in predicting the crossing action exactly 16 frames (same as the sliding window used for densenet) before it takes place (around 0.5 seconds). </li>
+                      <li>M2: The percentage of "crossing" prediction in the 16 frames proceeding the action </li>
+                      <li>M3: The accuracy in predicting the crossing action in the frame where the action actually takes place. </li>
+                    </ol>
+                    For example, if the GT tells us that the pedestrian is crossing at frame 17, we will check whether our models predict "crossing" at frame 1, at frame 17, and the percentage of "crossing" predictions between frames 1 and 16. We went through the same procedure for each pedestrian in all the videos to calculate the accuracy and the average percentage. We excluded the pedestrians whose crossing action was happening before the 16th frame, as it wouldn’t have been possible to calculate M2 and M3.
+
                   </div>
                   <br />
+                  <img src={this.resultImages[0]} width="45%" alt="metrics" />
                   <div className={classes.body}>
-                    For Model B, DeepSORT was able to detect pedestrians crossing several frames before Model A was able to. The below GIFs show that Model B was able to detect a pedestrian crossing 4 frames before Model A.
+                    From the metrics we conclude that Model C gives the highest performance.
                   </div>
-                  < br />
-                  <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                      <Paper className={classes.paper}>
-                        <img src={this.resultGifs[0]} alt="modelB1" width="90%" /><br />
-                        SORT detects pedestrian at Frame 83
-                      </Paper>
-
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Paper className={classes.paper}>
-                        <img src={this.resultGifs[1]} alt="modelB2" width="90%" /><br />
-                        DeepSORT detects pedestrian at Frame 79
-                      </Paper>
-
-                    </Grid>
-                  </Grid>
                   <br />
 
                 </div> : ''}
